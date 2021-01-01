a38c334fad9b7783fa1d83dc61f21048b19daf03
Sherly1001 <Sherly1001@users.noreply.github.com>
2021-01-01 23:14:25 +0700

    navbar

--- public/js/index.js
+++ public/js/index.js
@@ -2,8 +2,18 @@ document.addEventListener('DOMContentLoaded', e => {
   Node.prototype.on = Node.prototype.addEventListener;
   const $ = document.querySelector.bind(document);
   const $$ = document.querySelectorAll.bind(document);
+  const print = console.log;
 
-  $('#search').on('submit', e => {
-    e.preventDefault();
-  })
+  let lg = $('#login');
+  for (let i = 0; i < lg.children.length; ++i) {
+    lg.children[i].on('click', e => {
+      e.preventDefault();
+      show_login(i);
+    })
+  }
+
+  const show_login = i => {
+    alert(i);
+  }
+  delete Node.prototype.on;
 })
--- routers/router.js
+++ routers/router.js
@@ -6,4 +6,8 @@ router.get('/', (req, res) => {
   res.render('index', {title: 'DBA Project'});
 })
 
+router.get('/tutors', (req, res) => {
+  res.render('tutors', {title: 'Tìm kiếm gia sư', tutors: null});
+})
+
 module.exports = router;
